cbbd7ce70cb5218afff54b91cda2fada8327dd91
mrunal08 <mrunalg96@gmail.com>
2021-09-14 11:26:51 +0530

    Math Operations-Addition,Subtraction,Division Lamda Expression

--- LambdaFunction.java
+++ LambdaFunction.java
@@ -3,15 +3,15 @@ public class LambdaFunction {
     public static void main(String[] args) {
 
         //addition operation
-        MathOperation addition = (firstNum,secondNum) ->(firstNum+secondNum);
+        IMathOperation addition = (firstNum,secondNum) ->(firstNum+secondNum);
         System.out.println("Addition : "+ addition.calculate(20,30));
 
         //Subtraction Operation
-        MathOperation subtraction=(firstNum, secondNum) ->(firstNum-secondNum);
+        IMathOperation subtraction=(firstNum, secondNum) ->(firstNum-secondNum);
         System.out.println("Subtraction : "+subtraction.calculate(60,30));
 
         //Division Operation
-        MathOperation division=(firstNum, secondNum) ->(firstNum/secondNum);
+        IMathOperation division=(firstNum, secondNum) ->(firstNum/secondNum);
         System.out.println("Division : "+division.calculate(60,30));
 
 
